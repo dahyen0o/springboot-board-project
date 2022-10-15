90d0340573e0f89017866a4a2397c18c71793998
sen <seeunchoi99@gmail.com>
2022-10-15 18:05:46 +0900

    🎉 스프링부트 프로젝트 초기 셋팅

---
+++ src/main/java/com/fastcamp/projectboard/FastcampProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcamp.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcamp/projectboard/FastcampProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcamp.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
