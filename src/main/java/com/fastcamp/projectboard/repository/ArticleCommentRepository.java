b978d7afbe755854532f4064c160591b708a182b
sen <seeunchoi99@gmail.com>
2023-02-13 00:31:03 +0900

    ✨ Repository에 querydsl로 api 검색 옵션 추가
    QuerydslPredicateExecutor, QuerydslBinderCustomizer 추가

--- src/main/java/com/fastcamp/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcamp/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,32 @@
 package com.fastcamp.projectboard.repository;
 
 import com.fastcamp.projectboard.domain.ArticleComment;
+import com.fastcamp.projectboard.domain.QArticle;
+import com.fastcamp.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        // listing 하지 않은 property(column)은 검색에서 제외
+        bindings.excludeUnlistedProperties(true);
+        // 검색 listing에 추가
+        bindings.including(root.content,root.createdAt, root.createdBy);
+        // title을 검색할 때 하나의 단어로만(first) & 대소문자 구분없이 검색
+        // + StringExpression::likeIgnoreCase = like ''
+        //   StringExpression::containsIgnoreCase = like '%%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- src/main/java/com/fastcamp/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcamp/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package com.fastcamp.projectboard.repository;
 
 import com.fastcamp.projectboard.domain.Article;
+import com.fastcamp.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // 기본 검색 기능 추가 (부분 검색 X, 대소문자 구분 없음)
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // listing 하지 않은 property(column)은 검색에서 제외
+        bindings.excludeUnlistedProperties(true);
+        // 검색 listing에 추가
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        // title을 검색할 때 하나의 단어로만(first) & 대소문자 구분없이 검색
+        // + StringExpression::likeIgnoreCase = like ''
+        //   StringExpression::containsIgnoreCase = like '%%'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
