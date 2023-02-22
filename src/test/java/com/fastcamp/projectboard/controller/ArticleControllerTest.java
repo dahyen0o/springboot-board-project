278cb93b28cae4f16fd5ac778718de82a0b6c1f7
sen <seeunchoi99@gmail.com>
2023-02-23 00:22:00 +0900

    ✅ ArticleControllerTest 미디어 타입 검사 업데이트

--- src/test/java/com/fastcamp/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcamp/projectboard/controller/ArticleControllerTest.java
@@ -47,7 +47,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"))
                 .andExpect(view().name("articles/detail"));
@@ -61,7 +61,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -73,7 +73,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
