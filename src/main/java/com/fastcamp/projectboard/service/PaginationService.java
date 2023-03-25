75a4354eafc60ffe21e4fe21da3fa4029bbc5285
sen <seeunchoi99@gmail.com>
2023-03-26 00:14:49 +0900

    ✨ 페이지네이션 서비스 구현 및 테스트

---
+++ src/main/java/com/fastcamp/projectboard/service/PaginationService.java
@@ -0,0 +1,21 @@
+package com.fastcamp.projectboard.service;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaginationService {
+
+    private static final int BAR_LENGTH = 5;
+
+    public List<Integer> getPaginationBarNumbers(int currentPageNumber, int totalPages) {
+        int startNumber = Math.max(0, currentPageNumber - BAR_LENGTH / 2);
+        int endNumber = Math.min(totalPages, startNumber + BAR_LENGTH);
+        return IntStream.range(startNumber, endNumber).boxed().toList();
+    }
+
+    public int currentBarLength() {
+        return BAR_LENGTH;
+    }
+}
