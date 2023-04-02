008d70ede24a8a5260ffd3ba5a8899bbee5db0ed
sen <seeunchoi99@gmail.com>
2023-04-02 14:00:50 +0900

    🐛 코드의 오타 수정

--- src/main/java/com/fastcamp/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcamp/projectboard/controller/ArticleController.java
@@ -57,7 +57,7 @@ public class ArticleController {
                 articleService.getArticleWithComments(articleId));
 
         map.addAttribute("article", article);
-        map.addAttribute("articleComments", article.articleCommentsResponses());
+        map.addAttribute("articleComments", article.articleCommentsResponse());
         map.addAttribute("totalCount", articleService.getArticleCount());
 
         return "articles/detail";
